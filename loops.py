1cbc68b8d0598ee494e917a40830cfc79744438e
neetu3j <138584632+neetu3j@users.noreply.github.com>
2025-08-10 00:27:29 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+list_of_cloud= ['aws', 'azure', 'gcp', 'ibm', 'oracle']
+cloud= "gcp"
+print(list_of_cloud)
+
+
+#add a new cloud to the list
+#add a new cloud selesforce
+list_of_cloud.append("salesforce")
+
+#add a new cloud oracale
+list_of_cloud.append("oracle")
+
+print(list_of_cloud)
+
+list_of_cloud.insert(2, "alibaba")
+print(list_of_cloud)
