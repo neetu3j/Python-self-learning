ca3a9c43bc7de439557ac2898da1bea48e8f6e1d
neetu3j <138584632+neetu3j@users.noreply.github.com>
2025-08-10 00:29:28 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# Take user input and convert to integers right away
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Addition
+sum_of_num = num1 + num2
+print("Sum of two numbers:", sum_of_num)
+
+# Subtraction
+diff_of_num = num1 - num2
+print("Difference of two numbers:", diff_of_num)
+
+# Multiplication
+prod_of_num = num1 * num2
+print("Product of two numbers:", prod_of_num)
+
+# Division
+div_of_num = num1 / num2
+print("Division of two numbers:", div_of_num)
+
