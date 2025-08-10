3e6ee0604fe48158c89486b7722d100c78aa81b8
neetu3j <138584632+neetu3j@users.noreply.github.com>
2025-08-10 16:55:29 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -6,7 +6,7 @@ import datetime
 def backup_files(source, destination):
     today = datetime.date.today()
     backup_files =os.path.join(destination, f"backup_{today}.tar.gz")
-    shutil.make_archive(backup_files, 'gztar', source)
+    shutil.make_archive(backup_files.replace('.tar.gz',''), 'gztar', source)
 
 
 source = "C:/Users/neetu/OneDrive/Documents/Python-workshop-practice"
